67f4128ba9761aec69af67520c61ebda4059abe3
NobodyBlank <112726497+NobodyBlank@users.noreply.github.com>
2023-03-10 18:47:29 +0800

    Add files via upload

---
+++ test_1.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+    //float a = 0, b = 0;
+    //float sum = 0;
+    //printf("请输入两个任意位数的数字，两个数字中间要有空格\n");
+    //scanf("%f %f", &a, &b);
+    //sum = a + b;
+    //printf("和为 = %f", sum);
+    //
+    //printf("\a");//唤醒电脑的蜂鸣，会叮咚叮咚两声
+    //
+    //int a=0, b=0;
+    //scanf("%d%d", &a, &b);
+    //printf("%d",a + b);
+
+    //double a = 9 / 2.0;//除法要想得到小数必须分号两边有一个小数
+    //printf("%f", a);
+    
+    //int a =0;
+    //printf("%d", ~a);
+
+    //int a = 1;
+    //int  b = (++a) + (++a) + (++a) + a;
+    //printf("%d，%d", a, b);//输出结果为4,16
+
+    //int a = 1;
+    //int  b = (a++) + (a++) + (a++) + a;
+    //printf("%d，%d", a, b);//输出结果为4,4  //以上两次结果说明无论是先给a+1后使用还是先使用后给a+1，都是先把整个一行的程序中的+1加完再使用或者先使用完再+1
+
+    
+
+    return 0;
+}
